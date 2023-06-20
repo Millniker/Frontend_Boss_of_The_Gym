5c0b86341b84a88acab2671cd4c241ade47ee11b
Vinnichenko-Ivan <fox4080@gmail.com>
2023-06-20 22:06:37 +0700

    Exception

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -2,29 +2,39 @@ import axios from "axios";
 import {logout} from "../store/userReducer";
 import {profile} from "./profileApi";
 import {BASE_URL} from "../utils/consts";
+import swal from 'sweetalert';
 
 export const registration = (email,login,name, password)=>{
     return async dispatch =>{
-    try {
-        const response = await axios.post(BASE_URL+`user/register/`, {
-            email,
-            login,
-            name,
-            password
-        })
-
-    }
-    catch (e){
-        //TODO событие для вылета информации, что надо поддтвердить почту
-        console.log(e)
-        alert(e)
-    }
+        try {
+            const response = await axios.post(BASE_URL+`user/register/`, {
+                email,
+                login,
+                name,
+                password
+            })
+            swal("Вы успешно зарегестрированы.")
+        }
+        catch (e){
+            if(e.response.data.message === "login already used"){
+                swal("Ошибка","Логин уже используется", "warning");
+            }
+            else if(e.response.data.message === "email already used"){
+                swal("Ошибка","Почта уже используется", "warning");
+            }
+            else if(e.code === "ERR_BAD_REQUEST") {
+                swal("Ошибка","Ошибка валидации", "warning");
+            }
+            else {
+                swal("Ошибка","Ошибка", "warning");
+            }
+            //TODO событие для вылета информации, что надо поддтвердить почту
+        }
     }
 }
 export const login = (login, password)=>{
     return async dispatch => {
         try {
-            console.log(login,password)
             const response = await axios.post(BASE_URL + `user/sing-in/`, {
                 login,
                 password
@@ -33,14 +43,24 @@ export const login = (login, password)=>{
             localStorage.setItem(`restoreToken`,response.data.restoreToken)
             dispatch(profile())
         } catch (e) {
-            alert(e)
+            if(e.response.data.message === "bad login or password") {
+                swal("Ошибка","Неверный пароль или логин", "warning");
+            }
+            else if(e.response.data.message === "not confirmed") {
+                swal("Ошибка","Вы не подтверждены. Проверьте почту");
+            }
+            else if(e.code === "ERR_BAD_REQUEST") {
+                swal("Ошибка","Ошибка валидации", "warning");
+            }
+            else {
+                swal("Ошибка","Ошибка", "warning");
+            }
         }
     }
 }
 export const userLogout = ()=>{
     return async dispatch => {
         try {
-
             dispatch(logout())
         } catch (e) {
             alert(e)
